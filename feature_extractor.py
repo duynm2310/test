b14ff67e519b0b4e624d5febe6197eba2db317d0
Lê Châu Giang <96758705+ChauGiang2210@users.noreply.github.com>
2025-05-06 23:03:16 +0700

    deploy

--- feature_extractor.py
+++ feature_extractor.py
@@ -1,10 +1,11 @@
 import torch
 import torchvision.transforms as transforms
-import torchvision.models as models
 from PIL import Image
 import os
 
-resnet = torch.load('train/resnet50_feature_extractor.pth')
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+resnet = torch.load('train/resnet50_feature_extractor.pth', map_location=device)
+resnet.to(device)
 
 #transform ảnh đầu vào
 Transforms = transforms.Compose([
@@ -20,8 +21,8 @@ def Feature_Extraction_img(img_path):
     if not file_name.lower().endswith(('.png', '.jpg', '.jpeg')): return
     img = Image.open(img_path)
     img = Transforms(img)
-    batch = torch.unsqueeze(img, 0)
+    batch = torch.unsqueeze(img, 0).to(device)
     with torch.no_grad():
       fc7_features = resnet(batch)
-      features.append(fc7_features.squeeze())
+      features.append(fc7_features.squeeze().cpu())
     return features
